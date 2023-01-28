047346dda5a76a12c5ea1bea340cfff518b14f30
Afnan Ferdousi <afnanferdousi550@gmail.com>
2023-01-29 02:40:40 +0600

    signup and jwt

--- index.js
+++ index.js
@@ -13,15 +13,50 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1qjysd6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//  JSON WEB TOKEN
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
 async function run() {
+    await client.connect();
     try {
-        
+        const userCollection = client.db('power_hack').collection('userCollection');
+
+        // POST USER
+        //post my profile
+        app.post('/api/registration', async (req, res) => {
+            const emailExists = await userCollection.findOne({ email: req.body.email });
+            if (emailExists) {
+                return res.status(400).json({ message: "Email already exists" });
+            }
+            else {
+                const email = req.body.email;
+                const user = req.body;
+                const result = await userCollection.insertOne(user);
+                const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+                console.log(user)
+                res.send({ result, token });
+            }
+        })
+
     } finally {
-        
+
     }
 }
 run().catch(err => {
@@ -29,7 +64,7 @@ run().catch(err => {
 })
 
 app.get('/', (req, res) => {
-  res.send(`
+    res.send(`
     <p>
         <h1>Power hack server is running</h1>
     </p>
@@ -37,7 +72,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Power hack app listening on port ${port}`)
+    console.log(`Power hack app listening on port ${port}`)
 })
 
 //Export the express api
